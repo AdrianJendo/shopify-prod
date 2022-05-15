0a01613ef423d5b40da56b967fae7883ca78df97
Adrian Jendo <ajendo@uwaterloo.ca>
2022-05-15 18:38:21 -0400

    Add sqllite3

---
+++ constants.js
@@ -0,0 +1,9 @@
+export const DB_NAME = "inventory_tracker.db";
+export const TABLE_NAME = "inventories";
+export const CITIES = [
+	"Vancouver",
+	"Waterloo",
+	"Toronto",
+	"Montreal",
+	"Edmonton",
+];
--- pages/api/items.js
+++ pages/api/items.js
@@ -1,43 +1,73 @@
-const cities = ["Vancouver", "Waterloo", "Toronto", "Montreal", "Edmonton"];
+import sqlite3 from "sqlite3";
+import { DB_NAME, TABLE_NAME, CITIES } from "constants.js";
 
-const inventoryItems = [
-	{
-		id: 1,
-		item: "pants",
-		city: "Vancouver",
-		weather: "rainy",
-	},
-	{
-		id: 2,
-		item: "pants",
-		city: "Vancouver",
-		weather: "rainy",
-	},
-	{
-		id: 3,
-		item: "pants",
-		city: "Vancouver",
-		weather: "rainy",
-	},
-	{
-		id: 4,
-		item: "pants",
-		city: "Vancouver",
-		weather: "rainy",
-	},
-];
+// Helper function for creating db
+const createDatabase = () => {
+	const newdb = new sqlite3.Database(DB_NAME, (err) => {
+		if (err) {
+			console.log("Getting error " + err);
+			exit(1);
+		}
+		newdb.exec(
+			`
+			CREATE TABLE ${TABLE_NAME} (
+				item TEXT NOT NULL,
+				city TEXT NOT NULL,
+				stock INTEGER NOT NULL,
+				PRIMARY KEY (item, city)
+			);
+			INSERT INTO ${TABLE_NAME} (item, city, stock)
+				VALUES ('Pants', 'Vancouver', 100),
+					('Clothes', 'Toronto', 200),
+					('Shoes', 'Montreal', 0);
+				`
+		);
+	});
+};
 
-let idCount = 5;
+const inventoryItems = [];
 
-export default function handler(req, res) {
+// Connect to db
+const db = new sqlite3.Database(DB_NAME, sqlite3.OPEN_READWRITE, (err) => {
+	if (err && err.code == "SQLITE_CANTOPEN") {
+		createDatabase();
+		return;
+	} else if (err) {
+		console.log("Getting error " + err);
+		exit(1);
+	}
+});
+
+export default async function handler(req, res) {
 	if (req.method === "POST") {
-		inventoryItems.push({
-			id: idCount++,
-			item: req.body.item,
-			city: req.body.city,
-			weather: "TDB",
+		const insertValues = `('${req.body.item}', '${req.body.city}' , ${
+			req.body.stock || 0
+		})`;
+		return new Promise((resolve) => {
+			db.exec(
+				`INSERT INTO ${TABLE_NAME} (item, city, stock)
+						VALUES ${insertValues};
+					`,
+				(err) => {
+					if (err) {
+						res.status(500).json({ msg: err });
+						resolve();
+					} else {
+						db.all(
+							`SELECT * FROM ${TABLE_NAME};`,
+							function (err, rows) {
+								if (err) {
+									res.status(500).json({ msg: err });
+								} else {
+									res.status(200).json({ items: rows });
+								}
+								resolve();
+							}
+						);
+					}
+				}
+			);
 		});
-		res.status(200).json({ items: inventoryItems });
 	} else if (req.method === "PUT") {
 	} else if (req.method === "DELETE") {
 		const deleteId = req.body.id;
